53520dd89b15b53e81b8852753b6bd47f168f1a5
Engjell <engjellbislimi1@gmail.com>
2020-02-23 18:06:45 -0500

    adding axios and basic register components

---
+++ client/src/components/Register.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1> Register</h1>
+        <input 
+            type="email" 
+            name="email"
+            placeholder="email"
+            v-model="email"
+        />
+        <input 
+            type="password" 
+            name="password"
+            v-model="password"
+            placeholder="password"
+        />
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+    export default  {
+        data () {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            register () {
+                console.log('button clicked')
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
