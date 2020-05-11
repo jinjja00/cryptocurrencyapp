a1ea90d481c8d565c0e9107cc139e7d8afbbe112
Engjell <engjellbislimi1@gmail.com>
2020-05-11 13:22:57 -0400

    change third party api call

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -1,46 +1,38 @@
 <template>
     <div>
        <header class="background">
-         <div class="wave-container">
-             <h1>Get Your Crypto News ! </h1>
-             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#f3f4f5" fill-opacity="1" d="M0,96L60,128C120,160,240,224,360,208C480,192,600,96,720,53.3C840,11,960,21,1080,37.3C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-              </svg>
-         </div>
+          <v-container>
+            <v-row justify="center">
+                <v-data-table
+                    :headers="headers"
+                    :items="crypto.cryptoNews"
+                    class="mr-5"
+                    :hide-default-footer="true"
+                    light
+                  >
+                    <template slot="item" slot-scope="props">
+                      <tr @click="handleClick(props.item)">
+                        <!-- <v-tooltip left>
+                          <template v-slot:activator="{ on }">  v-on="on"-->
+                              <td nowrape="true" style="text-transform: uppercase">{{ props.item.symbol }}</td>
+                          <!-- </template>
+                          <span><td nowrape="true">{{ props.item.name }}</td></span>
+                        </v-tooltip> -->
+                        <td nowrape="true">
+                          <v-chip color="grey darken-1" dark>{{ roundDecimal(props.item.current_price) }} $</v-chip>
+                        </td>
+                        <td nowrape="true">{{ roundDecimal(props.item.price_change_percentage_24h) }} %</td>
+                        <td nowrape="true">{{ props.item.last_updated| moment("MMMM Do YYYY, h:mm:ss a") }}</td>
+                      </tr>
+                    </template>
+                  </v-data-table>
+                  <div> </div>
+                <v-card class="mr-5">
+                  <apexchart width="500" type="candlestick" :options="options" :series="series"></apexchart>
+                </v-card>
+            </v-row>
+          </v-container>
        </header>
-       <v-container>
-         <v-row justify="center">
-           <v-app>
-             <v-data-table
-                :headers="headers"
-                :items="crypto.cryptoNews"
-                class="elevation-1"
-                :hide-default-footer="true"
-                dark
-              >
-                <template slot="item" slot-scope="props">
-                  <tr>
-                    <!-- <v-tooltip left>
-                      <template v-slot:activator="{ on }">  v-on="on"-->
-                          <td nowrape="true">{{ props.item.symbol }}</td>
-                      <!-- </template>
-                      <span><td nowrape="true">{{ props.item.name }}</td></span>
-                    </v-tooltip> -->
-                    <td nowrape="true">
-                      <v-chip color="grey darken-1" dark>{{ roundDecimal(props.item.quote.USD.price) }} $</v-chip>
-                      </td>
-                    <td nowrape="true">{{  roundDecimal(props.item.quote.USD.percent_change_24h) }} %</td>
-                    <td nowrape="true">{{ props.item.last_updated| moment("MMMM Do YYYY, h:mm:ss a") }}</td>
-                  </tr>
-                </template>
-              </v-data-table>
-            </v-app>
-            <div>
-               <apexchart width="500" type="candlestick" :options="options" :series="series"></apexchart>
-            </div>
-         </v-row>
-       </v-container>
-       {{crypto.cryptoQuoteHistory}}
     </div>
 </template>
 
@@ -78,11 +70,11 @@
             headers: [
               {
                 text: 'Name',
-                align: 'start'
+                align: 'start',
+                value : 'symbol'
               },
               {
                 text: 'Price (USD)'
-          
               },
               {
                 text: 'Percent (24h)'
@@ -95,7 +87,7 @@
         },
         beforeCreate: function () {
           store.dispatch('crypto/fetchCrypto')
-          store.dispatch('crypto/fetchCryptoQuoteHistory')
+
         },
         mounted : function ()  {
   
@@ -104,8 +96,11 @@
           ...mapState(['crypto'])
         },
         methods: {
-          roundDecimal(num) {
+          roundDecimal (num) {
             return parseFloat(num).toFixed(2)
+          },
+          handleClick (cryptoRow) {
+            console.log(cryptoRow)
           }
         }
     }
@@ -117,36 +112,9 @@
         margin: 15rem 1rem 2rem 1rem;
         text-shadow: 2px 2px #A9A9A9
     }
-    .wave-container > svg {
-        display: block;
-    }
-    .wave-container {
-        position: relative;
-        color: #f3f4f5;
-        text-align: center; 
-    }
+   
     .background {
         background: url('../assets/keyboardbanner.jpg') no-repeat center center fixed;
         background-size: cover;
-        height: 80vh;
-        overflow: hidden;
-    }
-    .section-description {
-        background-color: black;
-        height: 70vh;
-    }
-    .wave-container > svg{
-      display: block;
-      transform-origin: bottom;
-      animation: animateWave 1000ms cubic-bezier(0.23, 1, 0.32, 1) forwards;
-    }
-
-    @keyframes animateWave {
-      0% {
-        transform: scale(1,0);
-      }
-      100% {
-        transform: scale(1,1);
-      }
     }
 </style>
--- server/src/controllers/CryptoController.js
+++ server/src/controllers/CryptoController.js
@@ -1,5 +1,8 @@
 const config  = require('../config/config')
 const rp = require('request-promise')
+const CoinGecko = require('coingecko-api')
+
+const CoinGeckoClient = new CoinGecko()
 
 let requestOptions = {
     method: 'GET',
@@ -14,35 +17,29 @@ let requestOptions = {
 
 module.exports = {
     async getCrypto (req, res) {
-        const request ={
-            ...requestOptions,
-            url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-            qs: { 
-                'start': '1',
-                'limit': '10',
-                'convert': 'USD'
-            }
-          }
-      
-        rp(request).then(response => {
-           res.send(response.data)
-          }).catch((err) => {
-            console.log('API call error:', err.message);
-          });
+          await CoinGeckoClient.coins.markets(
+            { vs_currency: "usd",
+              order: "market_cap_desc",
+              per_page: 10,
+              page: 1,
+              sparkline: true,
+              price_change_percentage: "24h"})
+            .then(response => {
+              res.send(response.data)
+            })
+            .catch(error => {
+              console.log(error)
+            })
     },
     async getCryptoQuoteHistory(req, res) {
-        const request ={
+        /* const request = {
             ...requestOptions,
             url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical',
             qs: { 
                 'id': '1'
             }
-          }
+          } */
      
-        rp(request).then(response => {
-            res.send(response.data)
-           }).catch((err) => {
-             console.log('API call error:', err.message);
-           });
+    
     }
 }
