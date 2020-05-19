26bb1eb7f2c51cdf0af4c3815ab7ac3a746441fa
Engjell <engjellbislimi1@gmail.com>
2020-05-19 15:13:33 -0400

    design a bit

--- client/src/views/CoinInformation.vue
+++ client/src/views/CoinInformation.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
-        <v-container fluid>
-
+        <v-container fluid> 
+            <v-row justify="center">
+                <v-col cols="8" md="4">
+                     <v-img width="150" :src="currentCoinInformation.image.large" aspect-ratio="1"></v-img>
+                 
+                </v-col>
+                <v-col cols="6" md="4">
+                    <v-card
+                        class="pa-2"
+                        outlined
+                        tile>
+                        .col-6 .col-md-4
+                    </v-card>
+                 </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
@@ -21,6 +34,7 @@
         },
         mounted () {
             this.currentCoinInformation = this.$store.state.crypto.coinInformation
+            console.log(this.currentCoinInformation)
         },
         computed: {
             ...mapState(['crypto'])
