3ad2112aa54fbc0567bef7007f2d1b72d6e69d63
Engjell <engjellbislimi1@gmail.com>
2020-06-24 19:52:31 -0400

    redesigning homepage

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -3,38 +3,55 @@
         <header class="background">
           <nav-bar></nav-bar>
           <div class="wave-container">
-              <h1>Your crypto news!</h1>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                  <path fill="#f3f4f5" fill-opacity="1" d="M0,96L60,128C120,160,240,224,360,208C480,192,600,96,720,53.3C840,11,960,21,1080,37.3C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-              </svg>
+                <h1>Your crypto news!</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#f3f4f5" fill-opacity="1" d="M0,96L60,128C120,160,240,224,360,208C480,192,600,96,720,53.3C840,11,960,21,1080,37.3C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                </svg>
           </div>
         </header>
         <v-container fluid class="pa-0 ma-0">
             <div class="wave-container">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#000" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,128C640,139,800,181,960,176C1120,171,1280,117,1360,90.7L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
-              </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#000" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,128C640,139,800,181,960,176C1120,171,1280,117,1360,90.7L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+                </svg>
             </div>
-            <v-row>
-          <v-col class="col-md-4 ml-6">
-            <h2>Look at your favorite cryptocurrencies!</h2>
-          </v-col>
-          <v-col class="col-md-2 offset-md-3">
-            <v-card dark justify="center"> 
-                <v-list>
-                    <v-list-item v-for="coin in coins" :key="coin.title">
-                       <v-list-item-content>
-                        <v-list-item-title v-text="coin.title"></v-list-item-title>
-                      </v-list-item-content>
+            <v-row justify="space-around" align="center">
+                <v-col class="col-md-2 offset-md-2">
+                    <img svg-inline :src="require('@/assets/cryptologo/undraw_crypto.svg')" height="500" width="500"/>
+                </v-col>
+                <v-row>
+                    <v-col class="col-md-6 offset-md-4">
+                        <h2 class="text-uppercase font-size-custom">Follow the trend</h2>
+                    </v-col>
+                </v-row>
+            </v-row>
+            <v-row class="black" justify="space-around" align="center">
+                <v-col>
+                    <v-card max-width="300"> 
+                            <v-list>
+                                <v-list-item v-for="coin in coins" :key="coin.title">
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="coin.title"></v-list-item-title>
+                                </v-list-item-content>
 
-                      <v-list-item-avatar>
-                        <v-img :src="coin.logo"></v-img>
-                      </v-list-item-avatar>
-                    </v-list-item>
-                </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
+                                <v-list-item-avatar>
+                                    <v-img :src="coin.logo"></v-img>
+                                </v-list-item-avatar>
+                                </v-list-item>
+                            </v-list>
+                        <v-list-item-subtitle>
+                            <v-btn
+                                block 
+                                outlined
+                                color="black"
+                                :to="{name: 'Coins'}"
+                            >
+                                Browse more..
+                            </v-btn>
+                        </v-list-item-subtitle>
+                    </v-card>
+                </v-col>  
+            </v-row>
         </v-container>
         <footercrypto/>
     </div>
@@ -52,11 +69,11 @@
           data () {
             return {
               coins: [
-                { title: 'Bitcoin', logo:  require('@/assets/cryptologo/btc.png')  },
-                { title: 'Ethereum', logo: require('@/assets/cryptologo/eth.png')  },
-                { title: 'Tether', logo: require('@/assets/cryptologo/tet.png')  },
-                { title: 'XRP', logo: require('@/assets/cryptologo/xrp.svg')  },
-                { title: 'Bitcoin Cash', logo: require('@/assets/cryptologo/btcash.png') },
+                { title: 'Bitcoin', logo:  require('@/assets/cryptologo/btc.png')},
+                { title: 'Ethereum', logo: require('@/assets/cryptologo/eth.png')},
+                { title: 'Tether', logo: require('@/assets/cryptologo/tet.png')},
+                { title: 'XRP', logo: require('@/assets/cryptologo/xrp.png')},
+                { title: 'Bitcoin Cash', logo: require('@/assets/cryptologo/btcash.png')},
               ]
             }
           },
@@ -66,6 +83,9 @@
 </script>
 
 <style scoped>
+    .font-size-custom {
+        font-size: 3em;
+    }
     h1 {
         font-size: 5rem;
         margin: 20rem 1rem 2rem 1rem;
