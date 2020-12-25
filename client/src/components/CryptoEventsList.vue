f7215ad3bd7958335541159e30450aea7779dd6e
Engjell <engjellbislimi1@gmail.com>
2020-12-25 13:53:50 -0500

    css

--- client/src/components/CryptoEventsList.vue
+++ client/src/components/CryptoEventsList.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
         <v-card dark justify="center">
-            <v-card-title class="headline">Upcoming Events</v-card-title>
-            <v-list-item v-for="(event, index) in crypto.cryptoEventIncoming.data" :key="index" :href="event.website" target=”_blank”>
-                <v-list-item-content>
-                    <v-list-item-title v-text="event.title">Single-line item</v-list-item-title>
-                    <v-list-item-subtitle>{{event.start_date}} &bull; {{event.end_date}} </v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
+            <v-card-title class="headline text-center">Upcoming Events</v-card-title>
+            <template v-if="crypto.cryptoEventIncoming.count != 0">
+                <v-list-item v-for="(event, index) in crypto.cryptoEventIncoming.data" :key="index" :href="event.website" target=”_blank”>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="event.title">Single-line item</v-list-item-title>
+                        <v-list-item-subtitle>{{event.start_date}} &bull; {{event.end_date}} </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
+            <template v-else>
+                <v-list-item-subtitle class="text-center">No events at the moment</v-list-item-subtitle>
+            </template>
         </v-card>
     </div>
 </template>
